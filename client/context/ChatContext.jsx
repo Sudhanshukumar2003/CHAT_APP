81055f2692c7fb55ad8d9e7e601350d8626f2711
Sudhanshukumar2003 <sudhanshukumarslss02@gmail.com>
2025-06-26 01:23:11 +0530

    removed mongoose set get import

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import toast from "react-hot-toast";
-import { get, set } from "mongoose";
+
 
 export const ChatContext = createContext();
 
